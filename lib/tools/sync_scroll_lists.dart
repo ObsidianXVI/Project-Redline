032d96b2e39a70fcd4403eaf25103566b8b235dd
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-11-14 15:58:53 +0800

    add animation function for `sync_scroll_lists`

--- lib/tools/sync_scroll_lists.dart
+++ lib/tools/sync_scroll_lists.dart
@@ -4,6 +4,32 @@ mixin SynchronisedScrollLists {
   final PageController controller1 = PageController();
   final PageController controller2 = PageController();
 
-  void controller1Callback(int pos) => controller2.jumpToPage(pos);
-  void controller2Callback(int pos) => controller1.jumpToPage(pos);
+  /// If set, this [Duration] will be used for both controllers. Leave null to
+  /// pass different [Duration]s to both controller animations.
+  Duration? standardisedDuration;
+
+  /// If set, this [Curve] will be used for both controllers. Leave null to
+  /// pass different [Curve]s to both controller animations.
+  Curve? standardisedCurve;
+
+  void controller1Callback(
+    int pos, {
+    Duration? duration,
+    Curve? curve,
+  }) =>
+      controller2.animateToPage(
+        pos,
+        duration: standardisedDuration ?? duration ?? Duration.zero,
+        curve: standardisedCurve ?? curve ?? Curves.linear,
+      );
+  void controller2Callback(
+    int pos, {
+    Duration? duration,
+    Curve? curve,
+  }) =>
+      controller1.animateToPage(
+        pos,
+        duration: standardisedDuration ?? duration ?? Duration.zero,
+        curve: standardisedCurve ?? curve ?? Curves.linear,
+      );
 }
